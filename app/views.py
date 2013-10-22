d890d46c7c898a544f626975247ab59dd4161ed7
sergio <sergiohurtadoramirez@gmail.com>
2013-10-22 13:35:08 -0500

    chapter 5

--- app/views.py
+++ app/views.py
@@ -10,8 +10,8 @@ def current_datetime(request):
     return render_to_response('current_datetime.html', locals())
 
 def hours_ahead(request, offset):
-    offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    assert False
-    return HttpResponse(html)
+    hour_offset = int(offset)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+    #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    #return HttpResponse(html)
+    return render_to_response('hours_ahead.html', locals())
--- misitio/settings.py
+++ misitio/settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'books',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
